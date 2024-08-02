d7f47bd0ecbb6174e9dc6a8c409dd2fe664805f2
Andrew Pettigrew <devdrew10@gmail.com>
2024-08-02 16:11:38 -0400

    Update the middleware for the route protection

--- src/middleware.ts
+++ src/middleware.ts
@@ -17,19 +17,13 @@ export async function middleware(request: NextRequest) {
   const federatedAuthTest = await authenticateAccountRouteRequest(request);
   const isFederatedAuthenticated = federatedAuthTest === null;
 
-
-
   if (path === home && (isCogntioAuthenticated || isFederatedAuthenticated)) {
-    console.log("authenticated user should be redirected to the dashboard");
     return NextResponse.redirect(new URL("/dashboard", request.nextUrl));
   }
 
   // Check if it's an third party authorize callback
   if (path === "/third-party-authorize") {
     const searchParams = request.nextUrl.searchParams;
-    console.log(`searchParams = ${searchParams}`);
-    console.log(`searchParams.has("code") = ${searchParams.has("code")}`);
-    console.log(`searchParams.has("state") = ${searchParams.has("state")}`);
     if (searchParams.has("code") && searchParams.has("state")) {
       // E.g. http://localhost:3002/third-party-authorize?code=<string>&state=<string>
       return response;
@@ -40,14 +34,14 @@ export async function middleware(request: NextRequest) {
   }
 
   if (protectedRoutes.includes(path)) {
-    console.log("User trying to access a protected route");
-
     if (!isFederatedAuthenticated || !isFederatedAuthenticated) {
-      console.log("User is not authenticated, redirecting to login");
       return NextResponse.redirect(new URL("/login", request.nextUrl));
     }
   }
 
+  if (path === home) {
+    return NextResponse.redirect(new URL("/login", request.nextUrl));
+  }
 
   return response;
 }
